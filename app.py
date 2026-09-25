b663c9b45fc02fec6fc8f9f6259fde5728b95df2
Zury <zury@local>
2026-09-25 20:18:29 +0700

    Make application port configurable via env variable

--- app.py
+++ app.py
@@ -138,4 +138,5 @@ else:
         return RedirectResponse(url="/api")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=7860)
+    port = int(os.environ.get("PORT", 7860))
+    uvicorn.run(app, host="0.0.0.0", port=port)
